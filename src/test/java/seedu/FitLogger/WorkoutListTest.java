3929d722ac25284979c5530f578c04e4fa3ed22c
Javierseow <seowjackiejavier@gmail.com>
2026-03-15 01:29:30 +0800

    Fix imports to import individual methods

--- src/test/java/seedu/FitLogger/WorkoutListTest.java
+++ src/test/java/seedu/FitLogger/WorkoutListTest.java
@@ -1,10 +1,11 @@
 package seedu.FitLogger;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class WorkoutListTest {
     private WorkoutList list;
