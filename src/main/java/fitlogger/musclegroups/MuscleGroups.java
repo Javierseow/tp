35698b49a10798c68a3104f8386eab7112b7fdaf
Javierseow <seowjackiejavier@gmail.com>
2026-04-03 11:41:39 +0800

    Add muscle group enum

---
+++ src/main/java/fitlogger/musclegroups/MuscleGroups.java
@@ -0,0 +1,18 @@
+package fitlogger.musclegroups;
+
+public enum MuscleGroups {
+    DELTS,
+    PECS,
+    FOREARMS,
+    UPPER_BACK,
+    LOWER_BACK,
+    ABS,
+    LATS,
+    BICEPS,
+    TRICEPS,
+    TRAPS,
+    GLUTES,
+    QUADS,
+    HAMSTRING,
+    CALVES
+}
